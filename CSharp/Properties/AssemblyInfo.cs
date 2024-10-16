5519c907c4e8b59cbe0193642fdde2d95bde9305
vintasoft <alex@vintasoft.com>
2024-10-16 15:55:45 +0300

    Source codes update

--- CSharp/Properties/AssemblyInfo.cs
+++ CSharp/Properties/AssemblyInfo.cs
@@ -14,9 +14,9 @@ using System.Windows;
 [assembly: AssemblyTitle("VintaSoft WPF Document Viewer Demo")]
 [assembly: AssemblyDescription("Shows how to annotate images and extract text from PDF and DOCX documents.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("VintaSoft Ltd.")]
+[assembly: AssemblyCompany("VintaSoft Llc.")]
 [assembly: AssemblyProduct("VintaSoft Imaging .NET SDK")]
-[assembly: AssemblyCopyright("Copyright VintaSoft Ltd. 2024")]
+[assembly: AssemblyCopyright("Copyright VintaSoft Llc. 2024")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
