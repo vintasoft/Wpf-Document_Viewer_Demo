33d2f4392de766c4f976303750af7a5d114f8d96
vintasoft <alex@vintasoft.com>
2024-11-22 10:21:59 +0300

    Source codes update

--- CSharp/Properties/AssemblyInfo.cs
+++ CSharp/Properties/AssemblyInfo.cs
@@ -52,5 +52,5 @@ using System.Windows;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("12.5.9.1")]
-[assembly: AssemblyFileVersion("12.5.9.1")]
+[assembly: AssemblyVersion("12.5.10.1")]
+[assembly: AssemblyFileVersion("12.5.10.1")]
